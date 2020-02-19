1e8b2370720515b8c7a23c25444539a7670cae7c
karandas777 <karandas.21.08.98@gmail.com>
2020-02-19 19:17:51 +0530

    8th commit

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -5,7 +5,7 @@ export default class Error extends Component {
         return (
             <div className="display-4 holder p-4 text-center rounded my-5">
                 <i class="fa fa-exclamation-triangle mr-3"></i>
-                Something went wrong.
+                Error
             </div>
         )
     }
